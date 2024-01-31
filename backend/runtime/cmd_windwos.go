128e4ae53942448ea4ab0c83dc3e55e2d2093964
fasnow <40886524+fasnow@users.noreply.github.com>
2024-01-31 19:14:46 +0800

    修复无法打开文件夹的问题

---
+++ backend/runtime/cmd_windwos.go
@@ -0,0 +1,15 @@
+package runtime
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func HideCmdWindow(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+}
+
+func KillProcess(cmd *exec.Cmd) error {
+	return cmd.Process.Signal(os.Kill)
+}

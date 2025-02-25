9d663fc9b5d333b76dc40db394fbcb3ec21f01b6
fasnow <40886524+fasnow@users.noreply.github.com>
2025-02-25 16:48:14 +0800

    2.0.1

--- backend/service/service/aiqicha/bridge.go
+++ backend/service/service/aiqicha/bridge.go
@@ -9,7 +9,9 @@ import (
 	"fine/backend/database/repository"
 	service2 "fine/backend/service/service"
 	"fine/backend/service/service/exportlog"
+	"github.com/cenkalti/backoff/v4"
 	"math"
+	"time"
 )
 
 type Bridge struct {
@@ -70,26 +72,86 @@ func (r *Bridge) GetBranchList(pid string, pageNum int) (int64, []*Branch, error
 func (r *Bridge) ExportAllCopyright(pid string) (int64, error) {
 	exportID, outputAbsFilepath, err := r.exportLogBridge.Create("AiQiCha")
 	if err != nil {
+		r.app.Logger.Error(err)
 		return 0, err
 	}
 	go func() {
-		total, _, err := r.aiQiCha.GetCopyrightList(pid, 1, 1)
-		if err != nil {
-			r.app.Logger.Error(err)
+		total, err2 := backoff.RetryWithData(func() (int64, error) {
+			total, _, err2 := r.aiQiCha.GetCopyrightList(pid, 1, 1)
+			if err2 != nil {
+				r.app.Logger.Error(err2)
+				return 0, err2
+			}
+			return total, nil
+		}, service2.GetBackOffWithMaxRetries(10, 3*time.Second, 1))
+		if err2 != nil {
+			service2.SaveToExcel(err2, exportID, event.AiQiCha, r.app.Logger, func() error {
+				return nil
+			})
 			return
 		}
 		maxPageNum := int(math.Ceil(float64(total / 10)))
 		items := make([]*Copyright, 0)
 		for pageNum := 1; pageNum <= maxPageNum; pageNum++ {
-			_, list, err := r.aiQiCha.GetCopyrightList(pid, pageNum, 10)
-			if err != nil {
-				r.app.Logger.Error(err)
-				return
+			result, err3 := backoff.RetryWithData(func() ([]*Copyright, error) {
+				_, list, err3 := r.aiQiCha.GetCopyrightList(pid, pageNum, 10)
+				if err3 != nil {
+					r.app.Logger.Error(err3)
+					return nil, err3
+				}
+				return list, nil
+			}, service2.GetBackOffWithMaxRetries(10, 3*time.Second, 1))
+			if err3 != nil {
+				break
 			}
-			items = append(items, list...)
+			items = append(items, result...)
 		}
 		service2.SaveToExcel(nil, exportID, event.AiQiCha, r.app.Logger, func() error {
 			return r.aiQiCha.ExportCopyrights(items, outputAbsFilepath)
+		})
+	}()
+	return exportID, nil
+}
+
+func (r *Bridge) ExportAllBranch(pid string) (int64, error) {
+	exportID, outputAbsFilepath, err := r.exportLogBridge.Create("AiQiCha")
+	if err != nil {
+		r.app.Logger.Error(err)
+		return 0, err
+	}
+	go func() {
+		total, err2 := backoff.RetryWithData(func() (int64, error) {
+			total, _, err2 := r.aiQiCha.GetBranchList(pid, 1, 1)
+			if err2 != nil {
+				r.app.Logger.Error(err2)
+				return 0, err2
+			}
+			return total, nil
+		}, service2.GetBackOffWithMaxRetries(10, 3*time.Second, 1))
+		if err2 != nil {
+			service2.SaveToExcel(err2, exportID, event.AiQiCha, r.app.Logger, func() error {
+				return nil
+			})
+			return
+		}
+		maxPageNum := int(math.Ceil(float64(total / 1000)))
+		items := make([]*Branch, 0)
+		for pageNum := 1; pageNum <= maxPageNum; pageNum++ {
+			result, err3 := backoff.RetryWithData(func() ([]*Branch, error) {
+				_, list, err3 := r.aiQiCha.GetBranchList(pid, pageNum, 1000)
+				if err3 != nil {
+					r.app.Logger.Error(err3)
+					return nil, err3
+				}
+				return list, nil
+			}, service2.GetBackOffWithMaxRetries(10, 3*time.Second, 1))
+			if err3 != nil {
+				break
+			}
+			items = append(items, result...)
+		}
+		service2.SaveToExcel(nil, exportID, event.AiQiCha, r.app.Logger, func() error {
+			return r.aiQiCha.ExportBranches(items, outputAbsFilepath)
 		})
 	}()
 	return exportID, nil
